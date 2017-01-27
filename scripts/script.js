d583a183f2f11f6d5b123029eb935f7c1a0b0fd2
Brandon Pereira <brandonp@criticalmass.com>
2017-01-27 13:58:59 -0700

    font end work (disable next button till request completed)

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,26 @@
 import ServiceWorkerRegister from './sw-register';
 import Style from '../styles/style.scss';
 
-var sw = new ServiceWorkerRegister();
-sw.sw.ready.then(function() {
+var sw = new ServiceWorkerRegister().sw;
+var button = document.querySelector('button');
+var imgContainer = document.querySelector(".body");
+
+sw.ready.then(function() {
 	// Service Worker Installed and ready!
 	console.log("Client: Service Worker is ready");
-	var img = document.querySelector("img");
-	img.setAttribute('src', '/getrandomcatpic'); // FIXME: first load breaks :(
-	
-	document.querySelector("button").addEventListener('click', () => {
-		img.setAttribute('src', '/getrandomcatpic?' + Date.now());
+	loadImage();
+	button.addEventListener('click', () => {
+		button.setAttribute('disabled', true);
+		loadImage();
 	});
 });
+
+// Loads new image from our middleware API (also disabled button till complete)
+function loadImage() {
+	var img = new Image();
+	img.onload = function() {
+		imgContainer.replaceChild(img, document.querySelector('img'));
+		button.removeAttribute('disabled');
+	}
+	img.src = "/getrandomcatpic?" + Date.now();
+}
